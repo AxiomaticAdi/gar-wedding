0def25a2dd9b5e74b421f72b30fcdd3a9284364d
Adi <140542996+AxiomaticAdi@users.noreply.github.com>
2024-01-08 14:40:51 -0800

    Update page title and favicon

--- src/components/Page.tsx
+++ src/components/Page.tsx
@@ -1,6 +1,6 @@
 export default function Page({ children }: { children: React.ReactNode }) {
 	return (
-		<div className="flex flex-col items-center max-w-2xl font-sans mx-auto">
+		<div className="flex flex-col items-center max-w-2xl font-sans mx-auto text-center">
 			<div className="h-[10vh]"></div>
 			{children}
 		</div>
