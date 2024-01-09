3b8f5340c99cb86d0f8c692db2afafbf0b99f063
Adi <140542996+AxiomaticAdi@users.noreply.github.com>
2024-01-09 02:32:57 -0800

    Add hover effect

--- src/components/InfoBox.tsx
+++ src/components/InfoBox.tsx
@@ -8,7 +8,7 @@ export default function InfoBox({ title, description, onClick }: InfoBoxProps) {
 	return (
 		<button
 			onClick={onClick}
-			className="flex flex-col justify-center items-center bg-white rounded-md p-3 shadow"
+			className="flex flex-col justify-center items-center bg-white rounded-md p-3 shadow hover:bg-gray-100"
 		>
 			<h4 className="text-xl text-gray-500">{title}</h4>
 			<p>{description}</p>
