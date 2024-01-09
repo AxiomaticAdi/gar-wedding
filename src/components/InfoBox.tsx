4bc4b2cd8a768cc6928b07a34cda6f114b1b3f33
Adi <140542996+AxiomaticAdi@users.noreply.github.com>
2024-01-09 01:27:16 -0800

    Stable modal component and context

--- src/components/InfoBox.tsx
+++ src/components/InfoBox.tsx
@@ -1,13 +1,17 @@
 interface InfoBoxProps {
 	title: string;
 	description: string;
+	onClick?: () => void;
 }
 
-export default function InfoBox({ title, description }: InfoBoxProps) {
+export default function InfoBox({ title, description, onClick }: InfoBoxProps) {
 	return (
-		<div className="flex flex-col justify-center items-center bg-white rounded-md p-3 shadow">
+		<button
+			onClick={onClick}
+			className="flex flex-col justify-center items-center bg-white rounded-md p-3 shadow"
+		>
 			<h4 className="text-xl text-gray-500">{title}</h4>
 			<p>{description}</p>
-		</div>
+		</button>
 	);
 }
